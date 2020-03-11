db209e12bdbc6f848e862075386e4308839bb835
Unnati256 <44746910+Unnati256@users.noreply.github.com>
2020-03-11 22:11:26 +0530

    pseudo fifo

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -100,7 +100,7 @@ int do_noquantum(message *m_ptr)
 
 	rmp = &schedproc[proc_nr_n];
 	if (rmp->priority < MIN_USER_Q) {
-		rmp->priority += 1; /* lower priority */
+		rmp->priority -= 1; /* lower priority */
 	}
 
 	if ((rv = schedule_process_local(rmp)) != OK) {
@@ -323,7 +323,7 @@ static int schedule_process(struct schedproc * rmp, unsigned flags)
 		new_quantum, new_cpu)) != OK) {
 		printf("PM: An error occurred when trying to schedule %d: %d\n",
 		rmp->endpoint, err);
-	}
+	}		
 
 	return err;
 }
@@ -357,7 +357,7 @@ static void balance_queues(minix_timer_t *tp)
 	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
 		if (rmp->flags & IN_USE) {
 			if (rmp->priority > rmp->max_priority) {
-				rmp->priority -= 1; /* increase priority */
+				//rmp->priority -= 1; /* increase priority */
 				schedule_process_local(rmp);
 			}
 		}
